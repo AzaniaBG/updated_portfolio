6d4e7a8fb06a556c784c8202bed3020d8ac66773
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2021-08-13 12:10:04 -0700

    added nav link styles; added display none rulesets depending on mobile display; added classes to conditionally render mobile navbar based on media breakpoint; added script tag linked to js file in other html pages

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 
 const navMenu = document.querySelector("#nav-menu");
+const burgerMenu = document.querySelector(".fas fa-bars")
 const toggleMenu = () => {
     const navList = document.querySelector("ul");
     navList.classList.toggle("hidden");
