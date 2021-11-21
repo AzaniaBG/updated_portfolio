12ec99ec6d0c2893dcc458ed955c90844aab8c2e
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2021-11-21 14:25:43 -0800

    added scripts js file

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 
-const navMenu = document.querySelector("#nav-menu");
-const burgerMenu = document.querySelector(".fas fa-bars")
+// import * as ProjectsData from './node_modules/module.js';
+
+const navMenu = document.querySelector(".nav-menu-mobile");
+const burgerMenu = document.querySelector(".burger-menu");
+console.log(burgerMenu);
 const toggleMenu = () => {
-    const navList = document.querySelector("ul");
-    navList.classList.toggle("hidden");
-}
-navMenu.addEventListener('click', toggleMenu);
+    navMenu.classList.toggle("hidden");
+};
+burgerMenu.addEventListener('click', toggleMenu);
+
+// const ProjectsData = require('./node_modules/module');
+// console.log(ProjectsData.projects);
+
+
+
+
