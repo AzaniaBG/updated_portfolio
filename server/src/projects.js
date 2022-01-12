6d6b119818d627bd45907ec3c3f5f99c4e0c67fb
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-01-11 17:18:44 -0800

    created module projects

---
+++ server/src/projects.js
@@ -0,0 +1,47 @@
+'use strict'
+/* see: https://www.freecodecamp.org/news/node-module-exports-explained-with-javascript-export-function-examples/ */
+module.exports = {
+    "projects": [
+        {
+            id: 1,
+            projectName: "Spanish Quiz App",
+            liveLink: "https://azaniabg.github.io/quiz-app/",
+            image: "./screenshots/Quiz-App/Start_Screen_mobile.jpg",
+            alt: "Mobile screen view of Quiz App's Start Page.",
+            description: "Quiz App that tests intermediate knowledge of the Spanish language.",
+            tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
+            repo: "https://github.com/AzaniaBG/quiz-app"
+        },
+        {
+            id: 2,
+            projectName: "Horror House API",
+            liveLink: "https://azaniabg.github.io/Horror-House-V2/",
+            image: "./screenshots/Horror-House/HomeSearchOptions.png",
+            alt: "Scary house in background; search boxes in foreground.",
+            description: "View search results for a horror film, including YouTube trailers, or search for similar horror movies.",
+            tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
+            repo: "https://github.com/AzaniaBG/Horror-House-V2"
+        },
+        {
+            id: 3,
+            projectName: "Portfolio",
+            liveLink: "https://azaniabg.github.io/Portfolio/",
+            
+            alt: "ABG Portfolio",
+            description: "View information about Azania Baker-Garcia's projects, contact information, knowledge.",
+            tech: "1. HTML 5, CSS3, 2. a touch of JavaScript",
+            repo: "https://github.com/AzaniaBG/Portfolio"
+        },
+        {
+            id: 4,
+            projectName: "Projects",
+            liveLink: "unavailable",
+            
+            alt: "ABG Projects",
+            description: "View Azania Baker-Garcia's projects.",
+            tech: "1. React, 2. ES6, 3. CSS3, CSS Grid",
+            repo: "https://github.com/AzaniaBG/abg-projects"
+        },
+    ]
+
+}
