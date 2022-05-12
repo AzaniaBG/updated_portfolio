945c1faf151677278721057f07b01cadd4c55a97
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-05-12 15:22:08 -0700

    added projects file

--- server/src/projects.js
+++ server/src/projects.js
@@ -6,7 +6,7 @@ module.exports = {
             id: 1,
             projectName: "Spanish Quiz App",
             liveLink: "https://azaniabg.github.io/quiz-app/",
-            image: "./screenshots/Quiz-App/Start_Screen_mobile.jpg",
+            image: "./images/project_images/QuizAppCover.jpg",
             alt: "Mobile screen view of Quiz App's Start Page.",
             description: "Quiz App that tests intermediate knowledge of the Spanish language.",
             tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
@@ -16,7 +16,7 @@ module.exports = {
             id: 2,
             projectName: "Horror House API",
             liveLink: "https://azaniabg.github.io/Horror-House-V2/",
-            image: "./screenshots/Horror-House/HomeSearchOptions.png",
+            image: "./images/project_images/HorrorHouseCover.png",
             alt: "Scary house in background; search boxes in foreground.",
             description: "View search results for a horror film, including YouTube trailers, or search for similar horror movies.",
             tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
@@ -26,7 +26,7 @@ module.exports = {
             id: 3,
             projectName: "Portfolio",
             liveLink: "https://azaniabg.github.io/Portfolio/",
-            
+            image: "./images/project_images/Portfolio_Header_View_mobile.png",
             alt: "ABG Portfolio",
             description: "View information about Azania Baker-Garcia's projects, contact information, knowledge.",
             tech: "1. HTML 5, CSS3, 2. a touch of JavaScript",
@@ -36,11 +36,22 @@ module.exports = {
             id: 4,
             projectName: "Projects",
             liveLink: "unavailable",
-            
+            image: "./images/project_images/react_projectsTINY.png",
             alt: "ABG Projects",
             description: "View Azania Baker-Garcia's projects.",
             tech: "1. React, 2. ES6, 3. CSS3, CSS Grid",
             repo: "https://github.com/AzaniaBG/abg-projects"
+        },
+        {
+            id: 5,
+            projectName: "Demo Brochure Website",
+            liveLink: "https://rdb-site.vercel.app/",
+            image: "./images/project_images/demo_brochure_siteTINY.png",
+            alt: "Mobile homepage view with links",
+            description: "View Azania Baker-Garcia's projects.",
+            tech: "Front-end: JavaScript/ES6, React, HTML5, CSS3 Back-end: Node.js, PostgreSQL, Express Contributors",
+            contributors: "Haja Childs: https://www.linkedin.com/in/haja-childs-dev-md/",
+            repo: ""
         },
     ]
 
