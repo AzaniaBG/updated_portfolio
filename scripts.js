ec6091a24cd3df7721680ab2bc73a300f04c484b
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-05-14 09:53:43 -0700

    removed unused code and modified class to toggle hidden

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 
-// import * as ProjectsData from './node_modules/module.js';
-
-const navMenu = document.querySelector(".nav-menu-mobile");
+const navMenu = document.querySelector(".ul-nav");
 const burgerMenu = document.querySelector(".burger-menu");
-console.log(burgerMenu);
+
 const toggleMenu = () => {
     navMenu.classList.toggle("hidden");
 };
 burgerMenu.addEventListener('click', toggleMenu);
 
-// const ProjectsData = require('./node_modules/module');
-// console.log(ProjectsData.projects);
+
 
 
 
