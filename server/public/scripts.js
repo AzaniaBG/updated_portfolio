487c85466bed61c5b3e2ae5ed336c91342e9a7f7
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-08-11 16:26:47 -0700

    removed server diretory and contents

--- server/public/scripts.js
+++
@@ -1,9 +0,0 @@
-const navMenu = document.querySelector(".nav-menu-mobile");
-const burgerMenu = document.querySelector(".burger-menu");
-console.log(burgerMenu);
-const toggleMenu = () => {
-    navMenu.classList.toggle("hidden");
-};
-burgerMenu.addEventListener('click', toggleMenu);
-
-// console.log("hello from scripts");
