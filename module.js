bce9d98dfbe17b49ff607a909e6875f446c8606f
Elizabeth BakerGarcia <azaniazbaker@gmail.com>
2022-08-17 16:32:21 -0700

    added module with projects data

--- module.js
+++ module.js
@@ -4,7 +4,7 @@ const ProjectsData = {
             id: 1,
             projectName: "Spanish Quiz App",
             liveLink: "https://azaniabg.github.io/quiz-app/",
-            image: "./screenshots/Quiz-App/Start_Screen_mobile.jpg",
+            image: "./project_images/QuizAppCoverTINY.jpg",
             alt: "Mobile screen view of Quiz App's Start Page.",
             description: "Quiz App that tests intermediate knowledge of the Spanish language.",
             tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
@@ -14,7 +14,7 @@ const ProjectsData = {
             id: 2,
             projectName: "Horror House API",
             liveLink: "https://azaniabg.github.io/Horror-House-V2/",
-            image: "./screenshots/Horror-House/HomeSearchOptions.png",
+            image: "./project_images/HorroHouseTINY.png",
             alt: "Scary house in background; search boxes in foreground.",
             description: "View search results for a horror film, including YouTube trailers, or search for similar horror movies.",
             tech: "1. Ecmascript 5, ES6, 2. jQuery, 3.HTML5, CSS3",
@@ -24,7 +24,7 @@ const ProjectsData = {
             id: 3,
             projectName: "Portfolio",
             liveLink: "https://azaniabg.github.io/Portfolio/",
-            
+            image: "./project_images/Portfolio_Header_View_mobileTINY.png",
             alt: "ABG Portfolio",
             description: "View information about Azania Baker-Garcia's projects, contact information, knowledge.",
             tech: "1. HTML 5, CSS3, 2. a touch of JavaScript",
@@ -34,62 +34,24 @@ const ProjectsData = {
             id: 4,
             projectName: "Projects",
             liveLink: "unavailable",
-            
+            image: "./project_images/react_projectsTINY.png",
             alt: "ABG Projects",
             description: "View Azania Baker-Garcia's projects.",
             tech: "1. React, 2. ES6, 3. CSS3, CSS Grid",
             repo: "https://github.com/AzaniaBG/abg-projects"
         },
+        {
+            id: 5,
+            projectName: "Fullstack website",
+            liveLink: "https://www.robertdavidbaker.com/",
+            image: "./project_images/rdbMobileTINY.png",
+            alt: "Law Office of Robert David Baker website",
+            description: "Fullstack website built for attorney client.",
+            tech: "Frontend: 1. React, 2. ES6, 3. React-bootstrap\nBackend: Express, Node.js, PostgreSQL",
+            repo: "private repo"
+        },
     ]
 
 }
-const AfricanProverbs = {
-    "quotes": [
-        { 
-            id: 1, 
-            quote: "Examine what is said, not who is speaking.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 2, 
-            quote: "Knowledge is a garden. If it isn’t cultivated, you can’t harvest it.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 3, 
-            quote: "Knowledge is a garden. If it isn’t cultivated, you can’t harvest it.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 4, 
-            quote: "Tomorrow belongs to people who prepare for it today.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 5, 
-            quote: "To try and fail is not laziness.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 6, 
-            quote: "A feeble effort will not fulfill the self.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 7, 
-            quote: "Great people become great when others are sleeping.", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        { 
-            id: 7, 
-            quote: "A man who uses force is afraid of reasoning..", 
-            source: "https://parade.com/1100530/marynliles/african-proverbs/"
-        },
-        
 
-    ]
-
-
-}
-
-export { ProjectsData }; 
+module.exports = { ProjectsData }; 
